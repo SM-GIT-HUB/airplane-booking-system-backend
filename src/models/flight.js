e987b2045bf738d0269a7cfdfb6866173e708906
Soumik Majumder <soumikcode02@gmail.com>
2025-08-08 00:57:10 +0530

    feat: added a new flight model and migration using the following commands and added proper foriegn keys: cd src npx sequelize model:generate --name Flight --attributes flightNumber:string, airplaneId:integer, departureAirportId:integer, arrivalAirportId:integer, departureTime:date, arrivalTime:date, price:integer, boardingGate:string, totalSeats:integer npx sequelize:db:migrate

--- src/models/flight.js
+++ src/models/flight.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Airport extends Model {
+  class Flight extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,48 +11,67 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.City, {
-        foreignKey: 'cityId',
+      this.belongsTo(models.Airplane, {
+        foreignKey: 'airplaneId',
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
       })
 
-      this.hasMany(models.Flight, {
+      this.belongsTo(models.Airport, {
         foreignKey: 'departureAirportId',
-        sourceKey: 'code',
+        targetKey: 'code',
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
       })
-
-      this.hasMany(models.Flight, {
+      
+      this.belongsTo(models.Airport, {
         foreignKey: 'arrivalAirportId',
-        sourceKey: 'code',
+        targetKey: 'code',
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
       })
     }
   }
-  Airport.init({
-    name: {
+  Flight.init({
+    flightNumber: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
-    code: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
+    airplaneId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
-    address: {
+    departureAirportId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    arrivalAirportId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    departureTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    arrivalTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    boardingGate: {
       type: DataTypes.STRING,
     },
-    cityId: {
+    totalSeats: {
       type: DataTypes.INTEGER,
       allowNull: false
     }
   }, {
     sequelize,
-    modelName: 'Airport',
+    modelName: 'Flight',
   });
-  return Airport;
+  return Flight;
 };
