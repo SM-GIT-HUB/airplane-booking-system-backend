10406a29f7d64b5353bec40d8ff0e750fdbd27da
Soumik Majumder <soumikcode02@gmail.com>
2025-08-21 21:43:07 +0530

    fix: cleaned up flight-middlewares by DRY

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -3,6 +3,59 @@ const { StatusCodes } = require('http-status-codes')
 const { ErrorResponse } = require('../utils/common')
 const AppError = require('../utils/errors/app-error')
 
+const bodyContains = [
+    { key: "flightNumber" }, { key: "airplaneId", number: 1 }, { key: "departureAirportId" },
+    { key: "arrivalAirportId" }, { key: "price", number: 1 }, { key: "totalSeats", number: 1 }
+]
+
+const bodyContainsDates = ["departureTime", "arrivalTime"];
+const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+
+function validateDates(req, res)
+{
+    const times = { departureTime: new Date(req.body.departureTime), arrivalTime: new Date(req.body.arrivalTime) };
+    
+    for (const dstring of bodyContainsDates)
+    {
+        if (!req.body[dstring] || !dateRegex.test(req.body[dstring]) || isNaN(times[dstring].getTime()))
+        {
+            const errorResponse = new ErrorResponse();
+            errorResponse.message = "Something went wrong while creating flight";
+            errorResponse.error = new AppError([`${dstring} not found or invalid in the request`], StatusCodes.BAD_REQUEST);
+            
+            return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
+        }
+    }
+    
+    if (times.departureTime.getTime() >= times.arrivalTime.getTime())
+    {
+        const errorResponse = new ErrorResponse();
+        errorResponse.message = "Something went wrong while creating flight";
+        errorResponse.error = new AppError(["departureTime must be before arrivalTime"], StatusCodes.BAD_REQUEST);
+        
+        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
+    }
+}
+
+function validateContents(req, res)
+{
+    for (const obj of bodyContains)
+    {
+        const check1 = !req.body[obj.key];
+        const check2 = (obj.number && !check1 && isNaN(req.body[obj.key]));
+
+        if (check1 || check2)
+        {
+            const errorResponse = new ErrorResponse();
+            errorResponse.message = "Something went wrong while creating flight";
+
+            errorResponse.error = new AppError([`${obj.key} ${check2? "invalid" : "not found"} in the request`], StatusCodes.BAD_REQUEST);
+
+            return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
+        }
+    }
+}
+
 function validateCreateRequest(req, res, next)
 {
     if (!req.body)
@@ -14,91 +67,8 @@ function validateCreateRequest(req, res, next)
         return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
     }
 
-    if (!req.body.flightNumber)
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["flightNumber not found in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    if (!req.body.airplaneId || isNaN(req.body.airplaneId))
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["airplaneId not found or invalid in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    if (!req.body.departureAirportId)
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["departureAirportId not found in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    if (!req.body.arrivalAirportId)
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["arrivalAirportId not found in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
-    
-    const deptTime = new Date(req.body.departureTime);
-
-    if (!req.body.departureTime || !dateRegex.test(req.body.departureTime) || isNaN(deptTime.getTime()))
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["departureTime not found or invalid in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-    
-    const arrvTime = new Date(req.body.arrivalTime);
-
-    if (!req.body.arrivalTime || !dateRegex.test(req.body.arrivalTime) || isNaN(arrvTime.getTime()))
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["arrivalTime not found or invalid in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    if (deptTime.getTime() >= arrvTime.getTime())
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["departureTime must be before arrivalTime"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    if (!req.body.price || isNaN(req.body.price))
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["price not found or invalid in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
-    }
-
-    if (!req.body.totalSeats || isNaN(req.body.totalSeats))
-    {
-        const errorResponse = new ErrorResponse();
-        errorResponse.message = "Something went wrong while creating flight";
-        errorResponse.error = new AppError(["totalSeats not found or invalid in the request"], StatusCodes.BAD_REQUEST);
-        
-        return res.status(StatusCodes.BAD_REQUEST).json(errorResponse);
+    if (validateContents(req, res) || validateDates(req, res)) {
+        return;
     }
 
     next();
