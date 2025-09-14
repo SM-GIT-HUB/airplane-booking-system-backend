1194dfccb38bad66ea812ce64897cbbbc7eb6efa
Soumik Majumder <soumikcode02@gmail.com>
2025-09-14 16:22:33 +0530

    FEAT: added transaction with locking for update seats api

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,3 +1,5 @@
+const db = require("../models")
+
 const CrudRepository = require('./crud-repository')
 const { Flight, Airplane, Airport, City } = require('../models')
 
@@ -24,16 +26,25 @@ class FlightRepository extends CrudRepository {
 
     async updateRemainingSeats(flightId, seats, dec = 1)
     {
-        const flight = await Flight.findByPk(flightId);
+        return await db.sequelize.transaction(async(t) => {
+            const flight = await Flight.findByPk(flightId, {
+                transaction: t,
+                lock: t.LOCK.UPDATE
+            })
 
-        if (parseInt(dec)) {
-            await flight.decrement('totalSeats', { by: seats });
-        }
-        else
-            await flight.increment('totalSeats', { by: seats });
-        
-        await flight.reload();
-        return flight;
+            if (!flight) {
+                throw new AppError("Not able to find the resource", StatusCodes.NOT_FOUND);
+            }
+
+            if (parseInt(dec)) {
+                await flight.decrement('totalSeats', { by: seats, transaction: t });
+            }
+            else
+                await flight.increment('totalSeats', { by: seats, transaction: t });
+            
+            await flight.reload({ transaction: t });
+            return flight;
+        })
     }
 }
 
